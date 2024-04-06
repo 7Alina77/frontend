e37ff6f61662134edc4285d84501196af9b7d469
Artem Asaev <117948944+AsaevArtemV@users.noreply.github.com>
2024-04-07 00:32:50 +0300

    In the report folder, сhanged the file css on css-module. Changed the layout structure.

--- src/pages/Report/Report.tsx
+++ src/pages/Report/Report.tsx
@@ -1,9 +1,8 @@
 import React, { FC, useRef } from 'react';
 import JsPdf from 'jspdf';
 import html2canvas from 'html2canvas';
-import './Report.css';
+import styles from './Report.module.css';
 import '../../components/Page/page.css';
-
 
 const Report: FC = (): React.ReactElement => {
   const pdfRef = useRef(null);
@@ -28,81 +27,89 @@ const Report: FC = (): React.ReactElement => {
   };
 
   return (
-    <section className='report'>
-      <div className='report__container' ref={pdfRef}>
-        <div className='report__section-data'>
-          <h2 className='report__title'>Данные проверяемого</h2>
-          <div className='report__subsection'>
-            <h3 className='report__subtitle'>Петров Василий Иванович</h3>
-            <ul className='report__info'>
-              <li className='report__info-text'>Дата рождения</li>
-              <li className='report__info-data'>12.01.1974</li>
+    <section className={styles.report}>
+      <div className={styles.container} ref={pdfRef}>
+        <div className={styles.section}>
+          <h2 className={styles.title}>Данные проверяемого</h2>
+          <div className={styles.subsection}>
+            <h3 className={styles.subtitle}>Петров Василий Иванович</h3>
+            <ul className={styles.info}>
+              <li className={styles.text}>Дата рождения</li>
+              <li className={styles.data}>12.01.1974</li>
             </ul>
-            <ul className='report__info'>
-              <li className='report__info-text'>Возраст</li>
-              <li className='report__info-data'>50 лет</li>
+            <ul className={styles.info}>
+              <li className={styles.text}>Возраст</li>
+              <li className={styles.data}>50 лет</li>
             </ul>
-            <ul className='report__info'>
-              <li className='report__info-text'>Паспорт</li>
-              <li className='report__info-data'>7684 896330</li>
+            <ul className={styles.info}>
+              <li className={styles.text}>Паспорт</li>
+              <li className={styles.data}>7684 896330</li>
             </ul>
-            <ul className='report__info'>
-              <li className='report__info-text'>Водительское удостоверение</li>
-              <li className='report__info-data'>63 93 970976</li>
+            <ul className={styles.info}>
+              <li className={styles.text}>Водительское удостоверение</li>
+              <li className={styles.data}>63 93 970976</li>
             </ul>
-            <ul className='report__info'>
-              <li className='report__info-text'>Дата выдачи водительского удостоверения</li>
-              <li className='report__info-data'>11.09.2015</li>
+            <ul className={styles.info}>
+              <li className={styles.text}>Дата выдачи водительского удостоверения</li>
+              <li className={styles.data}>11.09.2015</li>
             </ul>
           </div>
         </div>
-
-        <div className='report__section-check'>
-          <h2 className='report__title'>Результат проверки</h2>
-          <div className='report__subsection'>
-            <h3 className='report__subtitle-attention'>
-              Обратите внимание <span className='report__span' />
+        <div className={styles.sectionCheck}>
+          <h2 className={styles.title}>Результат проверки</h2>
+          <div className={styles.subsection}>
+            <h3 className={styles.subtitleAttention}>
+              Обратите внимание <span className={styles.span} />
             </h3>
-            <ul className='report__info-attention'>
-              <li className='report__info-text'>Исполнительное производство</li>
-              <li className='report__info-data-no-risk'>Найдено</li>
+            <ul className={styles.attention}>
+              <li className={styles.text}>Исполнительное производство</li>
+              <li className={styles.infoDataRisk}>Найдено</li>
             </ul>
-            <h3 className='report__subtitle-attention'>
-              Рисков не выявлено <span className='report__span-ok' />
+            <h3 className={styles.subtitleAttention}>
+              Рисков не выявлено <span className={styles.spanOk} />
             </h3>
-            <ul className='report__info-no-risk'>
-              <li className='report__info-text-no-risk'>Паспорт</li>
-              <li className='report__info-data-no-risk'>Действителен</li>
+            <ul className={styles.infoRisk}>
+              <li className={styles.infoTextRisk}>Паспорт</li>
+              <li className={styles.infoDataRisk}>Действителен</li>
             </ul>
-            <ul className='report__info-no-risk'>
-              <li className='report__info-text-no-risk'>ИНН</li>
-              <li className='report__info-data-no-risk'>648394753974</li>
+            <ul className={styles.infoRisk}>
+              <li className={styles.infoTextRisk}>ИНН</li>
+              <li className={styles.infoDataRisk}>648394753974</li>
             </ul>
-            <ul className='report__info-no-risk'>
-              <li className='report__info-text-no-risk'>Самозанятость</li>
-              <li className='report__info-data-no-risk'>Найдено</li>
+            <ul className={styles.infoRisk}>
+              <li className={styles.infoTextRisk}>Самозанятость</li>
+              <li className={styles.infoDataRisk}>Найдено</li>
             </ul>
-            <ul className='report__info-no-risk'>
-              <li className='report__info-text-no-risk'>Банкротство</li>
-              <li className='report__info-data-no-risk'>Не найдено</li>
+            <ul className={styles.infoRisk}>
+              <li className={styles.infoTextRisk}>Банкротство</li>
+              <li className={styles.infoDataRisk}>Не найдено</li>
             </ul>
-            <ul className='report__info-no-risk'>
-              <li className='report__info-text-no-risk'>Штрафы ГИБДД</li>
-              <li className='report__info-data-no-risk'>Не найдено</li>
+            <ul className={styles.infoRisk}>
+              <li className={styles.infoTextRisk}>Штрафы ГИБДД</li>
+              <li className={styles.infoDataRisk}>Не найдено</li>
             </ul>
-            <ul className='report__info-no-risk'>
-              <li className='report__info-text-no-risk'>Террористы и экстремисты</li>
-              <li className='report__info-data-no-risk'>Не найдено</li>
+            <ul className={styles.infoRisk}>
+              <li className={styles.infoTextRisk}>Террористы и экстремисты</li>
+              <li className={styles.infoDataRisk}>Не найдено</li>
             </ul>
           </div>
         </div>
       </div>
-      <div className='report__btn-container'>
-        <button type='submit' className='report__btn' onClick={handleDownloadPDF}>
+      <button type="submit" className={styles.btnNew}>
+        Новый отчет
+      </button>
+      <div className={styles.sendContainer}>
+        <form className={styles.form}>
+          <fieldset className={styles.fieldset}>
+            <label className={styles.label} htmlFor="email">
+              Введите электронную почту
+            </label>
+            <input className={styles.input} type="email" name="mail" id="email" required />
+          </fieldset>
+          <button className={styles.btnForm} type="submit" >Отправить на почту</button>
+        </form>
+        <button className={styles.btnPdf} type="submit" onClick={handleDownloadPDF}>
           Скачать PDF
-        </button>
-        <button type='submit' className='report__btn-new'>
-          Новый отчет
         </button>
       </div>
     </section>
